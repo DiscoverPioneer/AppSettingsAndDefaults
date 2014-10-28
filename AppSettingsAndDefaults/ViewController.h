19a85fa3a7f7e6a1ebff8ecef3cbbc7dcaadccf8
Phil <dplaxer25@aol.com>
2014-10-28 08:44:33 -0400

    Initial Commit

---
+++ AppSettingsAndDefaults/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppSettingsAndDefaults
+//
+//  Created by Phil Scarfi on 10/28/14.
+//  Copyright (c) 2014 Pioneer Mobile Application. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
